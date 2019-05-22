094bf1d17058e0d3949e0198a0e4dd23c0e8748f
Garret Allen <garret.allen@mail.helenacollege.edu>
2019-05-22 12:59:04 -0600

    New View Added

---
+++ Mobi_App_Project_BryonsChanges/Mobi_App_Project/Mobi_App_Project/Views/IndividualAssessmentHome.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mobi_App_Project.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class IndividualAssessmentHome : ContentPage
+	{
+		public IndividualAssessmentHome ()
+		{
+			InitializeComponent ();
+		}
+	}
+}

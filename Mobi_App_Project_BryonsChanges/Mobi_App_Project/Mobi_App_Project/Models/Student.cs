fbe94385489826fceb87e1e1d13b4ed936ce0ae6
James Bell <masterpiece0310@hotmail.com>
2019-05-22 13:42:04 -0600

    Modified Student Model

--- Mobi_App_Project_BryonsChanges/Mobi_App_Project/Mobi_App_Project/Models/Student.cs
+++ Mobi_App_Project_BryonsChanges/Mobi_App_Project/Mobi_App_Project/Models/Student.cs
@@ -13,7 +13,7 @@ namespace Mobi_App_Project.Models
         public string LastName { get; set; }
         public string MiddleName { get; set; }
         public int Age { get; set; }
-        public int Grade { get; set; }
+        public string Grade { get; set; }
        //public int ID { get; internal set; }
     }
 }
---
+++ Mobi_App_Project_BryonsChanges/Mobi_App_Project/Mobi_App_Project/ViewModels/NewStudentFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mobi_App_Project.Models;
+
+namespace Mobi_App_Project.ViewModels
+{
+    public class NewStudentFormViewModel : BaseViewModel
+    {
+        public Student Student { get; set; }
+
+        public NewStudentFormViewModel()
+        {
+
+        }
+    }
+}

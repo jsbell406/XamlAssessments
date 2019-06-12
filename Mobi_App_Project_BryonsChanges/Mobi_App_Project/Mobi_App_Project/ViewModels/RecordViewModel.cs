2659252640c50de72ae9ea3e7d23a97141705475
dream@DESKTOP-IBP62AT <dreamironchef@gmail.com>
2019-06-11 22:07:11 -0600

    Record.xmal, RecordViewModel, Record.cs created with two search boxes

---
+++ Mobi_App_Project_BryonsChanges/Mobi_App_Project/Mobi_App_Project/ViewModels/RecordViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mobi_App_Project.Models;
+namespace Mobi_App_Project.ViewModels
+{
+    class RecordViewModel
+    {
+        public ObservableCollection<AssessmentSession> SearchDateList { get; set; }
+        public ObservableCollection<AssessmentSession> DateList { get; set; }
+        public Command LoadDateCommand { get; set; }
+        public ICommand SearchCommand { get; set; }
+
+        private List<AssessmentSession> assessmentSession;
+        public List<AssessmentSession> AssessmentSession
+        {
+            get
+            {
+                return assessmentSession;
+            }
+            set
+            {
+                assessmentSession = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double searchedDate;
+        public double SearchedDate
+        {
+            get { return searchedDate; }
+            set { searchedDate = value; OnPropertyChanged(); }
+        }
+
+        public RecordViewModel()
+        {
+            Title = "Browse Session Date";
+            SearchDateList = new ObservableCollection<AssessmentSession>();
+            LoadDateCommand = new Command(async () => await ExecuteLoadDateCommand());
+
+            DateList = new ObservableCollection<AssessmentSession>();
+            AssessmentSession = App.AssesmentDB.GetItemsAsync().Result;
+            SearchCommand = new Command(async () => await SearchCommandExecute());
+
+        }
+
+        async Task SearchCommandExecute()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                DateList.Clear();
+                var tempRecords = assessmentSession.Where(s => s.SessionDate.Contains(SearchedDate));
+                foreach (var item in tempRecords)
+                {
+                    DateList.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        async Task ExecuteLoadStudentsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                SearchDateList.Clear();
+                var items = await App.AssesmentDB.GetItemsAsync();
+                foreach (var item in items)
+                {
+                    SearchDateList.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
--- Mobi_App_Project_BryonsChanges/Mobi_App_Project/Mobi_App_Project/Views/Record.xaml.cs
+++ Mobi_App_Project_BryonsChanges/Mobi_App_Project/Mobi_App_Project/Views/Record.xaml.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Mobi_App_Project.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Mobi_App_Project.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Record : ContentPage
-	{
-		public Record ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Record : ContentPage
+    {
+          
+        public Record()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new Record();
+        }
+        private async void SelectedDate(object sender, EventArgs e)
+        {
+            // Nav to assessment selection
+            //await Navigation.PushAsync(new NavigationPage(new AssessmentSelection()));         
+        }
+
+       
+    }
+	
 }

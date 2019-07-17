85053fdce7fffa5dbee4b61e9703b7c084a6f492
James Bell <masterpiece0310@hotmail.com>
2019-07-17 13:17:33 -0600

    fixing merge

--- Mobi_App_Project_BryonsChanges/Mobi_App_Project/Mobi_App_Project/Models/Result.cs
+++ Mobi_App_Project_BryonsChanges/Mobi_App_Project/Mobi_App_Project/Models/Result.cs
@@ -5,15 +5,10 @@ namespace Mobi_App_Project.Models
     public class Result
     {
         [PrimaryKey, AutoIncrement]
-<<<<<<< HEAD
-        public int ResultId { get; set; }      
-=======
         public int ResuldId { get; set; }      
         public int AssessmentSessionId { get; set; }
->>>>>>> dev
         public int AssesmentQuestionId { get; set; }
         public int QuestionId { get; set; }
         public string TextResults { get; set; }
-        public int AssessmentSessionId { get; set; }
     }
 }

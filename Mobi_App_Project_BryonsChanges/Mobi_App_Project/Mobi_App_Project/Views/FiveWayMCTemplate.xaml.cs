8e5ed8becf2af072e6a6d809ad99339c57a97025
James Bell <masterpiece0310@hotmail.com>
2019-07-30 23:51:42 -0600

    Updated Navigation during assessment, Each question is now served on its on with no ability to navigate other than completing the question. This pages have a simple animation that can be turned on or off. Question to Question has the animation last question to results is without the animation

--- Mobi_App_Project_BryonsChanges/Mobi_App_Project/Mobi_App_Project/Views/FiveWayMCTemplate.xaml.cs
+++ Mobi_App_Project_BryonsChanges/Mobi_App_Project/Mobi_App_Project/Views/FiveWayMCTemplate.xaml.cs
@@ -34,7 +34,7 @@ namespace Mobi_App_Project.Views
             }
             else
             {
-                await Navigation.PushAsync(new Results());
+                await Navigation.PushModalAsync(new Results(), false);
             }
         }
 
@@ -69,19 +69,19 @@ namespace Mobi_App_Project.Views
             switch (question.Qtype)
             {
                 case "5WayMC":
-                    await Navigation.PushAsync(new FiveWayMCTemplate(new FiveWayMCTemplateViewModel(question, assessmentQuestion)));
+                    await Navigation.PushModalAsync(new FiveWayMCTemplate(new FiveWayMCTemplateViewModel(question, assessmentQuestion)));
                     break;
                 case "3WayMC":
-                    await Navigation.PushAsync(new ThreeWayMCTemplate(new ThreeWayMCTemplateViewModel(question, assessmentQuestion)));
+                    await Navigation.PushModalAsync(new ThreeWayMCTemplate(new ThreeWayMCTemplateViewModel(question, assessmentQuestion)));
                     break;
                 case "2WayMC":
-                    await Navigation.PushAsync(new TwoWayMCTemplate(new TwoWayMCTemplateViewModel(question, assessmentQuestion)));
+                    await Navigation.PushModalAsync(new TwoWayMCTemplate(new TwoWayMCTemplateViewModel(question, assessmentQuestion)));
                     break;
                 case "SingleText":
-                    await Navigation.PushAsync(new SingleTextTemplate(new SingleTextTemplateViewModel(question, assessmentQuestion)));
+                    await Navigation.PushModalAsync(new SingleTextTemplate(new SingleTextTemplateViewModel(question, assessmentQuestion)));
                     break;
                 case "TripleText":
-                    await Navigation.PushAsync(new TripleTextTemplate(new TripleTextTemplateViewModel(question, assessmentQuestion)));
+                    await Navigation.PushModalAsync(new TripleTextTemplate(new TripleTextTemplateViewModel(question, assessmentQuestion)));
                     break;
                 default:
                     await Navigation.PushModalAsync(new AdminHome());
